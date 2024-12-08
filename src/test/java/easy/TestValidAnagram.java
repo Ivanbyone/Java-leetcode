a0175c7dee8b28c84b6cda3a123861c970147c3a
ivanbyone <ivanbyone@gmail.com>
2024-12-08 18:21:34 +0300

    Added ValidAnagram task

---
+++ src/test/java/easy/TestValidAnagram.java
@@ -0,0 +1,24 @@
+package easy;
+
+import org.example.easy.ValidAnagram;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestValidAnagram {
+    private static ValidAnagram testClass;
+
+    @BeforeAll
+    public static void init() {
+        testClass = new ValidAnagram();
+    }
+
+    @Test
+    public void test1() {
+        String testcase1 = "anagram";
+        String testcase2 = "nagaram";
+        boolean result = testClass.isAnagram(testcase1, testcase2);
+        assertTrue(result);
+    }
+}
